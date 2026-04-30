fb439c5b4c71a9531d8f6ccf925f85a0db4a2c68
ejcondorf88 <eliancondor2523658@gmail.com>
2026-04-30 02:50:15 -0500

    fix: dependecies and query native

--- ms-customer/src/main/java/com/bank/customer/infrastructure/persistence/repository/ClientJpaRepository.java
+++ ms-customer/src/main/java/com/bank/customer/infrastructure/persistence/repository/ClientJpaRepository.java
@@ -2,6 +2,7 @@ package com.bank.customer.infrastructure.persistence.repository;
 
 import com.bank.customer.infrastructure.persistence.entity.ClientJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +11,10 @@ import java.util.Optional;
 @Repository
 public interface ClientJpaRepository extends JpaRepository<ClientJpaEntity, Long> {
 
+    @Query("SELECT c FROM ClientJpaEntity c JOIN c.person p WHERE p.identification = :identification")
     Optional<ClientJpaEntity> findByIdentification(String identification);
 
+    @Query("SELECT COUNT(c) > 0 FROM ClientJpaEntity c JOIN c.person p WHERE p.identification = :identification")
     boolean existsByIdentification(String identification);
 
     List<ClientJpaEntity> findByActiveTrue();
